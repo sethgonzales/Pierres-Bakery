0d43e7b30e281fbcb2601eeb5e760e78871981e0
sethgonzales <sethgonzales157@gmail.com>
2023-09-22 14:02:51 -0700

    add order menu and console styling

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -8,42 +8,69 @@ namespace PierresBakery
   {
     static void Main()
     {
-      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.BackgroundColor = ConsoleColor.DarkYellow;
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~**~*~*~**~*~*~*");
       Console.WriteLine("Welcome to the Pierre's Bakery!");
       Console.WriteLine("We offer a beautiful selection of bread and pastries.");
-      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
-      Console.WriteLine("~~~~~~~~~~~~~~ Crazy deal alert!!! ~~~~~~~~~~~~~~");
-      Console.WriteLine("~~~ Buy two loaves of bread and get one free! ~~~");
-      Console.WriteLine("~~~~~ Buy three pastries and get one free! ~~~~~");
-      Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-      Console.WriteLine("How many loaves of bread would you like (enter a number)?");
-      string breadOrderString = Console.ReadLine();
-      Console.WriteLine("How many pastries would you like? (enter a number)");
-      string pastryOrderString = Console.ReadLine();
+      Console.WriteLine("");
+      Console.WriteLine("Menu:");
+      Console.WriteLine("---------------------");
+      Console.WriteLine("|Bread     |     $5 |");
+      Console.WriteLine("|Pastry    |     $2 |");
+      Console.WriteLine("---------------------");
+      Console.WriteLine("");
+      Console.WriteLine("Deal of the day!");
+      Console.WriteLine("Buy two, get one FREE on bread");
+      Console.WriteLine("Buy three, get one FREE on pastries");
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~**~*~*~**~*~*~*");
+      Console.WriteLine("Type 'yes' to continue, or any other key to exit");
+      string continueOn = Console.ReadLine();
 
-      int breadOrder;
-      int pastryOrder;
-
-      bool breadOrderValue = int.TryParse(breadOrderString, out breadOrder);
-      bool pastryOrderValue = int.TryParse(pastryOrderString, out pastryOrder);
-
-      if (breadOrderValue && pastryOrderValue)
+      if (continueOn.ToLower() == "yes")
       {
-        TotalCost newOrder = new TotalCost(breadOrder, pastryOrder);
-        int totalCost = newOrder.GetTotalCost();
-        Console.WriteLine("==================================================");
-        Console.WriteLine($"Your order comes out to be ${totalCost}.");
-        Console.WriteLine("==================================================");
+        Console.BackgroundColor = ConsoleColor.DarkMagenta;
+        Console.WriteLine("How many loaves of bread would you like? (enter a number)");
+        string breadOrderString = Console.ReadLine();
+        Console.WriteLine("How many pastries would you like? (enter a number)");
+        string pastryOrderString = Console.ReadLine();
+
+        int breadOrder;
+        int pastryOrder;
+
+        bool breadOrderValue = int.TryParse(breadOrderString, out breadOrder);
+        bool pastryOrderValue = int.TryParse(pastryOrderString, out pastryOrder);
+
+        if (breadOrderValue && pastryOrderValue)
+        {
+          TotalCost newOrder = new TotalCost(breadOrder, pastryOrder);
+          int totalCost = newOrder.GetTotalCost();
+          Console.WriteLine("==================================================");
+          Console.WriteLine($"Your order comes out to be ${totalCost}.");
+          Console.WriteLine("==================================================");
+        }
+        else
+        {
+          Console.WriteLine("==================================================");
+          Console.WriteLine("Please enter a numerical values such as '1'");
+          Console.WriteLine("rather than 'one' or '1.0' for each of your orders");
+          Console.WriteLine("==================================================");
+          Console.WriteLine("Type 'yes' to continue, or any other key to exit");
+          string restart = Console.ReadLine();
+          if (restart.ToLower() == "yes")
+          {
+            Main();
+          }
+          else
+          {
+            Console.WriteLine("Thanks for stopping by, we hope to see you again!");
+
+          }
+        }
       }
       else
       {
-        Console.WriteLine("==================================================");
-        Console.WriteLine("Please enter a numerical values such as '1'");
-        Console.WriteLine("rather than 'one' or '1.0' for each of your orders");
-        Console.WriteLine("==================================================");
-
+        Console.WriteLine("Thanks for stopping by, we hope to see you again!");
       }
     }
-
   }
 }
